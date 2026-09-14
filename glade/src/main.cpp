fe982bac9d337ff5bfefa6bacee002cef32f7e73
dongrang <andrewzeng1020@gmail.com>
2026-09-14 01:02:26 -0400

    shader added

--- glade/src/main.cpp
+++ glade/src/main.cpp
@@ -1,8 +1,10 @@
 #include <vertex/Vertex.h>
-
+#include <vertex/Shader.h>
+#include <vertex/Window.h>
 #include <cstdio>
 
 int main() {
     std::printf("glade on vertex %s\n", vertex::version());
+    
     return 0;
 }
--- vertex/include/vertex/Shader.h
+++ vertex/include/vertex/Shader.h
@@ -1,6 +1,23 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 namespace Vertex {
 
+    class Shader {
+        public:
+            Shader();
+            Shader(const std::string& vertFileName, const std::string& fragFileName);
+            void loadShader(const std::string& vertFileName, const std::string& fragFileName);
+            void supplyIntUniform(const std::string& uniformName,const std::vector<int>& vals);
+            void Bind();
+            ~Shader();
+        private:
+            unsigned int mShader{0};
+
+            std::string ReadFile(const std::string& filename);
+            
+    };
     
 } 
